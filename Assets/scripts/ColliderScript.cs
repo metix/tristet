88cab001e72a338679a5f8cc60a884c581f6b516
Ingo Simon <IngoSimon96@web.de>
2018-05-16 12:23:07 +0200

    player can be crushed

---
+++ Assets/scripts/ColliderScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderScript : MonoBehaviour {
+
+    public bool head;
+    public bool feet;
+    public bool forward;
+    public bool backward;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(head)
+            gameObject.transform.parent.GetComponent<PlayerCollisionScript>().head = true;
+        if(feet)
+            gameObject.transform.parent.GetComponent<PlayerCollisionScript>().feet = true;
+        if (forward)
+            gameObject.transform.parent.GetComponent<PlayerCollisionScript>().forward = true;
+        if (backward)
+            gameObject.transform.parent.GetComponent<PlayerCollisionScript>().backward = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (head)
+            gameObject.transform.parent.GetComponent<PlayerCollisionScript>().head = false;
+        if (feet)
+            gameObject.transform.parent.GetComponent<PlayerCollisionScript>().feet = false;
+        if (forward)
+            gameObject.transform.parent.GetComponent<PlayerCollisionScript>().forward = false;
+        if (backward)
+            gameObject.transform.parent.GetComponent<PlayerCollisionScript>().backward = false;
+    }
+}
---
+++ Assets/scripts/PlayerCollisionScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionScript : MonoBehaviour {
+
+    public bool feet;
+    public bool head;
+    public bool forward;
+    public bool backward;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((feet && head) || (forward && backward))
+        {
+            Destroy(gameObject);
+            //Game over
+        }
+	}
+}
