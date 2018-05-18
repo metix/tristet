7b430f1b8c06415548d25605a0d883ec698e7b6c
Ingo Simon <IngoSimon96@web.de>
2018-05-18 11:38:24 +0200

    added start Screen

---
+++ Assets/StartPanelScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StartPanelScript : MonoBehaviour {
+
+    public GameObject global;
+
+	// Use this for initialization
+	void Start () {
+        EventSystem.current.SetSelectedGameObject(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void close()
+    {
+        GetComponent<Canvas>().enabled = false;
+        global.GetComponent<GameFieldScript>().StartGame();
+    }
+}
